ff2e0872885cd25f33c0f4dd0df76afaf397b0e9
Larissa Galvão Nascimento <larissa_galvao@id.uff.br>
2023-04-10 11:15:20 -0300

    feat: criar usuarios; listar todos pelo username

--- src/index.js
+++ src/index.js
@@ -1,25 +1,50 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+ const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
-}
+  const {username} = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if(!user){
+    return response.status(400).json({error: "User not found!"});
+  } else {
+    request.user = user;
+    return next();
+  }
+};
 
 app.post('/users', (request, response) => {
   // Complete aqui
+  const {name, username} = request.body;
+  const userAlreadyExists = users.some((user) => user.username === username);
+  if(userAlreadyExists){
+    return response.status(400).json({error:"User already existis!"});
+  }
+  const user = {
+    id: uuidv4(), // precisa ser um uuid
+	  name, 
+	  username, 
+	  todos: []
+  };
+  users.push(user);
+  return response.status(201).json(user);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const {user} = request;
+  return response.json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
@@ -39,3 +64,5 @@ app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
 });
 
 module.exports = app;
+
+app.listen(3334);
