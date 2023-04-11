9f53542a83029a4ba03211cff7a2e25f6a42884d
Larissa Galvão Nascimento <larissa_galvao@id.uff.br>
2023-04-11 09:42:37 -0300

    feat: deletar contas pelo id do usuario

--- src/index.js
+++ src/index.js
@@ -66,14 +66,37 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const {id} = request.params;
+  const {user} = request;
+  const {title, deadline} = request.body;
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  todo.title = title;
+  todo.deadline = new Date(deadline);
+
+  return response.status(201).json(todo);
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const {user} = request;
+  const {id} = request.params;
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  todo.done = true;
+
+  return response.status(201);
+  
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const {user} = request;
+  const {id} = request.params;
+
+  users.splice(user, 1);
+
+  return response.status(200).json(users);
 });
 
 module.exports = app;
